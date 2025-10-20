49ad58f3e356811407145204b11419e8bbf0bdce
AhmedKhalaf-97 <ahmedkhalafx97@gmail.com>
2025-10-20 09:06:48 -0700

    Implemented the injectin of HTML and CSS of the extension into the webpage to display the AI Summarizer API result.

--- scripts/content.js
+++ scripts/content.js
@@ -23,18 +23,44 @@
 // When message receivd from service-worker.js, proceed.
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     if (message.action === 'summarize') {
-        console.log("message received.");
+        // document.getElementById("ai-result").innerText = await summarizePage(message.data);
 
-        document.getElementById("ai-result").innerText = await summarizePage(message.data);
+
     }
 });
 
-// injectHTML();
+injectHTML();
+
 
 async function injectHTML() {
     const injectedHTMLURL = chrome.runtime.getURL('injected_html.html');
     const injectedCSSURL = chrome.runtime.getURL('style.css');
-    const aiIconImg = chrome.runtime.getURL('images/icon-128.png');
+    const aiIconURL = chrome.runtime.getURL('images/icon-128.png');
+    const dragIconURL = chrome.runtime.getURL('images/drag-drop.png');
+
+    const draggableIframeWrapper = document.createElement("div");
+    draggableIframeWrapper.id = "draggableIframeWrapper";
+    draggableIframeWrapper.setAttribute("draggable", "true");
+    document.body.appendChild(draggableIframeWrapper);
+
+    const topBar = document.createElement("div");
+    topBar.id = "topBar";
+    draggableIframeWrapper.appendChild(topBar);
+
+    const aiIcon = document.createElement("img");
+    aiIcon.id = "ai-icon";
+    aiIcon.src = aiIconURL;
+    topBar.appendChild(aiIcon);
+
+    const dragIconImg = document.createElement("img");
+    dragIconImg.id = "dragIconImg";
+    dragIconImg.src = dragIconURL;
+    topBar.appendChild(dragIconImg);
+
+    const closeBtn = document.createElement("div");
+    closeBtn.id = "closeBtn";
+    topBar.appendChild(closeBtn);
+
 
     // Inject css styling.
     const cssLink = document.createElement("link");
@@ -43,19 +69,27 @@ async function injectHTML() {
     cssLink.href = injectedCSSURL;
     document.head.appendChild(cssLink);
 
+    const metaTagElement = document.createElement("meta");
+    metaTagElement.setAttribute("charset", "UTF-8");
+
     // Inject extension html.
+    const injectedIframe = document.createElement('iframe');
+    injectedIframe.id = "injected-iframe";
+    draggableIframeWrapper.appendChild(injectedIframe);
+
+
+    injectedIframe.contentDocument.head.appendChild(cssLink.cloneNode(false));
+    injectedIframe.contentDocument.head.appendChild(metaTagElement);
+
     await fetch(injectedHTMLURL).then(res => res.text()).then(data => {
-        const injectedDiv = document.createElement('div');
-        injectedDiv.id = "injected-div";
-
-        injectedDiv.innerHTML = data;
-
-        document.body.appendChild(injectedDiv);
-
+        injectedIframe.contentDocument.body.innerHTML = data;
     }).catch(e => console.error("Error loading extenstion HTML."));
 
+
     // Update the extension html icon image.
-    document.getElementById("ai-icon").src = aiIconImg;
+    // injectedIframe.contentDocument.getElementById("ai-icon").src = aiIconURL;
+
+
 }
 
 async function summarizePage(pageContent) {
@@ -77,5 +111,49 @@ async function summarizePage(pageContent) {
         const stream = await summarizer.summarize(pageContent);
         // summaryResultElement.textContent += stream;
         return stream;
+    }
+}
+
+const draggableElement = document.getElementById("draggableIframeWrapper");
+
+DragElement(draggableElement);
+
+function DragElement(element) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(element.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(element.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        element.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
     }
 }
--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "summarizeCMI") {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             chrome.tabs.sendMessage(tabs[0].id, { action: 'summarize', data: info.selectionText });
-            chrome.action.openPopup();
+            // chrome.action.openPopup();
         });
     }
 });
