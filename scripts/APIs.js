49a4e584c7cf3439af1b889a24d8da507e91836e
AhmedKhalaf-97 <ahmedkhalafx97@gmail.com>
2025-10-27 13:22:34 -0700

    Implemented Rewriter API

---
+++ scripts/APIs.js
@@ -0,0 +1,212 @@
+async function summarize(content) {
+    let output = "";
+
+    if ('Summarizer' in self) {
+        // The Summarizer API is supported.
+
+        const availability = await Summarizer.availability();
+        if (availability === 'unavailable') {
+            // The Summarizer API isn't usable.
+            output = "The Summarizer isn't available right now. Please try again.";
+        }
+        else {
+            const summarizer = await Summarizer.create({
+                type: "key-points",
+                length: "short",
+                format: "markdown"
+            });
+
+            try {
+                output = await summarizer.summarize(content);
+            }
+            catch (error) {
+                output = "Input is too large or the Summarizer is not available right now. Please try again.";
+            }
+        }
+    }
+    else {
+        output = "Your browser doesn't support the Summarizer API.";
+    }
+
+    return output;
+}
+
+
+async function proofread(content) {
+    let output = "";
+
+    if ('Proofreader' in self) {
+        const availability = await Proofreader.availability();
+
+        if (availability === 'unavailable') {
+            output = "The Proofreader isn't available right now. Please try again.";
+        }
+        else {
+            const proofreader = await Proofreader.create({
+                expectedInputLanguages: ['en'],
+            });
+
+            try {
+                const proofreadResult = await proofreader.proofread(content);
+                output = "<strong>Corrected text:</strong><br><br>";
+                output += proofreadResult.correctedInput;
+
+                output += "<br><br><hr><br><strong>Original text:</strong>"
+
+                let inputtedText = content;
+                let offset = 0;
+                for (const correction of proofreadResult.corrections) {
+                    inputtedText = inputtedText.slice(0, correction.startIndex + offset) + "<mark>" + inputtedText.slice(correction.startIndex + offset);
+                    offset += 6;
+                    inputtedText = inputtedText.slice(0, correction.endIndex + offset) + "</mark>" + inputtedText.slice(correction.endIndex + offset);
+                    offset += 7;
+                }
+
+                output += `<br><br>${inputtedText}`;
+
+                output += "<br><br><strong><i>*Corrections are highlighted above.</i></strong>";
+
+                output += "<br><br><strong>Corrections made:</strong>";
+
+                let i = 0;
+                for (const correction of proofreadResult.corrections) {
+                    output += `<br>${++i}. ${correction.correction}`;
+                }
+            }
+            catch (error) {
+                output = "Input is too large or the Proofreader is not available right now. Please try again.";
+            }
+        }
+    }
+    else {
+        output = "Your browser doesn't support the Proofreader API.";
+    }
+
+    return output;
+}
+
+async function detectLanguage(content) {
+    let output = "";
+
+    if ('LanguageDetector' in self) {
+        const availability = await LanguageDetector.availability();
+
+        if (availability === 'unavailable') {
+            output = "The Language Detector isn't available right now. Please try again.";
+        }
+        else {
+            const langDetector = await LanguageDetector.create();
+
+            try {
+                const detectionResults = await langDetector.detect(content);
+                output = detectionResults[0].detectedLanguage;
+            }
+            catch (error) {
+                output = "Input is too large or the Language Detector is not available right now. Please try again.";
+            }
+        }
+    }
+    else {
+        output = "Your browser doesn't support the Language Detector API.";
+    }
+
+    return output;
+}
+
+async function translate(content, sourceLang, targetLang) {
+    let output = "";
+
+    if ('Translator' in self) {
+        const availability = await Translator.availability({
+            sourceLanguage: sourceLang,
+            targetLanguage: targetLang,
+        });
+
+        if (availability === 'unavailable') {
+            output = "The Translator isn't available right now. Please try again.";
+        }
+        else if (availability === 'downloading') {
+            output = "The Translator is still downloading. Please try again.";
+            await Translator.create({
+                sourceLanguage: sourceLang,
+                targetLanguage: targetLang,
+            });
+        }
+        else if (availability === 'downloadable') {
+            output = "The Translator is still downloading. Please try again.";
+            await Translator.create({
+                sourceLanguage: sourceLang,
+                targetLanguage: targetLang,
+            });
+        }
+        else if (availability === 'available') {
+            const translator = await Translator.create({
+                sourceLanguage: sourceLang,
+                targetLanguage: targetLang,
+            });
+
+            try {
+                output = await translator.translate(content);
+            }
+            catch (error) {
+                output = "Input is too large or the Translator is not available right now. Please try again.";
+            }
+        }
+        else {
+            output = "The Translator is still downloading or isn't available right now. Please try again.";
+        }
+    }
+    else {
+        output = "Your browser doesn't support the Translator API.";
+    }
+
+    return output;
+}
+
+async function rewrite(content, tone, length) {
+    let output = "";
+
+    if ('Rewriter' in self) {
+        const availability = await Rewriter.availability();
+
+        if (availability === 'unavailable') {
+            output = "The Rewriter isn't available right now. Please try again.";
+        }
+        else if (availability === 'downloading') {
+            output = "The Rewriter is still downloading. Please try again.";
+            await Translator.create({
+                sourceLanguage: sourceLang,
+                targetLanguage: targetLang,
+            });
+        }
+        else if (availability === 'downloadable') {
+            output = "The Rewriter is still downloading. Please try again.";
+            await Translator.create({
+                sourceLanguage: sourceLang,
+                targetLanguage: targetLang,
+            });
+        }
+        else if (availability === 'available') {
+            const rewriter = await Rewriter.create({
+                tone: tone,
+                length: length,
+                format: "plain-text"
+            });
+
+            try {
+                output = await rewriter.rewrite(content);
+            }
+            catch (error) {
+                output = "Input is too large or the Rewriter is not available right now. Please try again.";
+            }
+        }
+        else {
+            output = "The Rewriter is still downloading or isn't available right now. Please try again.";
+        }
+    }
+    else {
+        output = "Your browser doesn't support the Rewriter API.";
+    }
+
+    return output;
+}
--- scripts/content.js
+++ scripts/content.js
@@ -56,7 +56,7 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     }
 });
 
-// initiateExtension(AIModels.TRANSLATOR);
+// initiateExtension(AIModels.REWRITER);
 
 async function initiateExtension(aiModel, selectedText) {
     let extensionUI = document.getElementById("draggableIframeWrapper") || null;
@@ -127,6 +127,9 @@ function openExtensionUI(extensionUI, aiModel) {
     extensionUI.style.left = "40%";
 }
 
+const submitBtnController = new AbortController();
+const submitBtnSignal = submitBtnController.signal;
+
 async function setExtensionUI(extensionUI, aiModel, selectedText) {
     const injectedIframe = document.getElementById("injected-iframe");
     const extensionTitleHeader = injectedIframe.contentDocument.getElementById("title-header");
@@ -141,11 +144,12 @@ async function setExtensionUI(extensionUI, aiModel, selectedText) {
     const translatorInputtedLang = injectedIframe.contentDocument.getElementById("translator-inputted-lang");
     const submitBtn = injectedIframe.contentDocument.getElementById("submit-btn");
 
+    submitBtnController.abort();
+
     extensionTitleHeader.textContent = "🤖" + aiModel;
 
     resultText.textContent = "";
     resultText.classList.add('disable-animations');
-
 
     if (aiModel === AIModels.SUMMARIZER) {
         originalTextTitle.classList.add('disable-element');
@@ -212,12 +216,17 @@ async function setExtensionUI(extensionUI, aiModel, selectedText) {
         submitBtn.addEventListener("click", () => {
             injectAIResult(AIModels.TRANSLATOR, selectedText, { sourceLang: detectedLang, targetedLang: targetLang });
             submitBtn.classList.add("submit-btn-disabled");
-        });
-
-
-        // add event listener for the submit btn to start injectAIResult()
+        }, { submitBtnSignal });
     }
     else if (aiModel === AIModels.REWRITER) {
+        const toneDropdownBtn = injectedIframe.contentDocument.getElementById("tone-dropdown-btn");
+        const toneDropdown = injectedIframe.contentDocument.getElementById("tone-dropdown-content");
+        const lengthDropdownBtn = injectedIframe.contentDocument.getElementById("length-dropdown-btn");
+        const lengthDropdown = injectedIframe.contentDocument.getElementById("length-dropdown-content");
+
+        toneDropdownBtn.textContent = "Select the Tone: ⤴️";
+        lengthDropdownBtn.textContent = "Select the Length: ⤴️";
+
         originalTextTitle.classList.remove('disable-element');
         originalText.classList.remove('disable-element');
         originalText.textContent = selectedText;
@@ -228,7 +237,41 @@ async function setExtensionUI(extensionUI, aiModel, selectedText) {
         rewriterInputGrp.classList.remove('disable-element');
         promptInputGrp.classList.add('disable-element');
 
-        submitBtn.classList.remove("submit-btn-disabled");
+        submitBtn.classList.add("submit-btn-disabled");
+
+        resultText.innerHTML = "<em><b>Make a selection below, then click the send button to start rewriting.</b></em>";
+
+        let tone = "";
+        let length = "";
+
+        const toneDropdownContent = toneDropdown.children;
+        for (const child of toneDropdownContent) {
+            child.addEventListener("click", () => {
+                tone = child.id;
+                toneDropdownBtn.textContent = "Tone: " + child.textContent + " ⤴️";
+
+                if (tone && length) {
+                    submitBtn.classList.remove("submit-btn-disabled");
+                }
+            });
+        }
+
+        const lengthDropdownContent = lengthDropdown.children;
+        for (const child of lengthDropdownContent) {
+            child.addEventListener("click", () => {
+                length = child.id;
+                lengthDropdownBtn.textContent = "Length: " + child.textContent + " ⤴️";
+
+                if (tone && length) {
+                    submitBtn.classList.remove("submit-btn-disabled");
+                }
+            });
+        }
+
+        submitBtn.addEventListener("click", () => {
+            injectAIResult(AIModels.REWRITER, selectedText, { _tone: tone, _length: length });
+            submitBtn.classList.add("submit-btn-disabled");
+        }, { submitBtnSignal });
     }
     else if (aiModel === AIModels.WRITER) {
         originalTextTitle.classList.add('disable-element');
@@ -283,7 +326,15 @@ async function injectAIResult(aiModel, selectedText, additionalParams) {
         }
         submitBtn.classList.remove("submit-btn-disabled");
     }
-
+    else if (aiModel === AIModels.REWRITER) {
+        try {
+            aiResultTextArea.innerHTML = await rewrite(selectedText, additionalParams._tone, additionalParams._length);
+        }
+        catch {
+            aiResultTextArea.innerHTML = "The Rewriter is still downloading. Please try again.";
+        }
+        submitBtn.classList.remove("submit-btn-disabled");
+    }
     aiResultTextArea.classList.add('disable-animations');
 }
 
@@ -320,169 +371,4 @@ function DragElement(element) {
         document.onmouseup = null;
         document.onmousemove = null;
     }
-}
-
-async function summarize(content) {
-    let output = "";
-
-    if ('Summarizer' in self) {
-        // The Summarizer API is supported.
-
-        const availability = await Summarizer.availability();
-        if (availability === 'unavailable') {
-            // The Summarizer API isn't usable.
-            output = "The Summarizer isn't available right now. Please try again.";
-        }
-        else {
-            const summarizer = await Summarizer.create({
-                type: "key-points",
-                length: "short",
-                format: "markdown"
-            });
-
-            try {
-                output = await summarizer.summarize(content);
-            }
-            catch (error) {
-                output = "Input is too large or the Summarizer is not available right now. Please try again.";
-            }
-        }
-    }
-    else {
-        output = "Your browser doesn't support the Summarizer API.";
-    }
-
-    return output;
-}
-
-
-async function proofread(content) {
-    let output = "";
-
-    if ('Proofreader' in self) {
-        const availability = await Proofreader.availability();
-
-        if (availability === 'unavailable') {
-            output = "The Proofreader isn't available right now. Please try again.";
-        }
-        else {
-            const proofreader = await Proofreader.create({
-                expectedInputLanguages: ['en'],
-            });
-
-            try {
-                const proofreadResult = await proofreader.proofread(content);
-                output = "<strong>Corrected text:</strong><br><br>";
-                output += proofreadResult.correctedInput;
-
-                output += "<br><br><hr><br><strong>Original text:</strong>"
-
-                let inputtedText = content;
-                let offset = 0;
-                for (const correction of proofreadResult.corrections) {
-                    inputtedText = inputtedText.slice(0, correction.startIndex + offset) + "<mark>" + inputtedText.slice(correction.startIndex + offset);
-                    offset += 6;
-                    inputtedText = inputtedText.slice(0, correction.endIndex + offset) + "</mark>" + inputtedText.slice(correction.endIndex + offset);
-                    offset += 7;
-                }
-
-                output += `<br><br>${inputtedText}`;
-
-                output += "<br><br><strong><i>*Corrections are highlighted above.</i></strong>";
-
-                output += "<br><br><strong>Corrections made:</strong>";
-
-                let i = 0;
-                for (const correction of proofreadResult.corrections) {
-                    output += `<br>${++i}. ${correction.correction}`;
-                }
-            }
-            catch (error) {
-                output = "Input is too large or the Proofreader is not available right now. Please try again.";
-            }
-        }
-    }
-    else {
-        output = "Your browser doesn't support the Proofreader API.";
-    }
-
-    return output;
-}
-
-async function detectLanguage(content) {
-    let output = "";
-
-    if ('LanguageDetector' in self) {
-        const availability = await LanguageDetector.availability();
-
-        if (availability === 'unavailable') {
-            output = "The Language Detector isn't available right now. Please try again.";
-        }
-        else {
-            const langDetector = await LanguageDetector.create();
-
-            try {
-                const detectionResults = await langDetector.detect(content);
-                output = detectionResults[0].detectedLanguage;
-            }
-            catch (error) {
-                output = "Input is too large or the Language Detector is not available right now. Please try again.";
-            }
-        }
-    }
-    else {
-        output = "Your browser doesn't support the Language Detector API.";
-    }
-
-    return output;
-}
-
-async function translate(content, sourceLang, targetLang) {
-    let output = "";
-
-    if ('Translator' in self) {
-        const availability = await Translator.availability({
-            sourceLanguage: sourceLang,
-            targetLanguage: targetLang,
-        });
-
-        if (availability === 'unavailable') {
-            output = "The Translator isn't available right now. Please try again.";
-        }
-        else if (availability === 'downloading') {
-            output = "The Translator is still downloading. Please try again.";
-            await Translator.create({
-                sourceLanguage: sourceLang,
-                targetLanguage: targetLang,
-            });
-        }
-        else if (availability === 'downloadable') {
-            output = "The Translator is still downloading. Please try again.";
-            await Translator.create({
-                sourceLanguage: sourceLang,
-                targetLanguage: targetLang,
-            });
-        }
-        else if (availability === 'available') {
-            const translator = await Translator.create({
-                sourceLanguage: sourceLang,
-                targetLanguage: targetLang,
-            });
-
-            try {
-                output = await translator.translate(content);
-            }
-            catch (error) {
-                output = "Input is too large or the Translator is not available right now. Please try again.";
-            }
-        }
-        else {
-            output = "The Translator is still downloading or isn't available right now. Please try again.";
-        }
-    }
-    else {
-        output = "Your browser doesn't support the Translator API.";
-    }
-
-    return output;
 }
